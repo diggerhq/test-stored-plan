ca36025630c2ea22d02f7f8b9e19ca9c1e34253b
Igor Zalutski <izalutski@gmail.com>
2024-01-31 12:52:41 +0000

    Update backend configuration

--- dev/main.tf
+++ dev/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
     backend "s3" {
-    bucket = "digger-test-stored-plan"              # Change if a different S3 bucket name was used for the backend 
+    bucket = "digger-test-stored-plan-new"              # Change if a different S3 bucket name was used for the backend 
     key    = "terraform/state/dev"
     region = "us-east-1"
   }
